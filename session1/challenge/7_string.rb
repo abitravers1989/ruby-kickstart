24213ecc6c58d24fbf8e577e48bbe7b929964a52
abitravers1989 <atravers4007@gmail.com>
2017-07-11 20:17:43 +0100

    Challenge 7 with sudo code/ english explination of what I want to happen in the place of some code

--- session1/challenge/7_string.rb
+++ session1/challenge/7_string.rb
@@ -1,8 +1,27 @@
 # given a string, return the character after every letter "r"
-# 
+#
 # pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
 # pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
 # pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
 
 def pirates_say_arrrrrrrrr(string)
+    return_this = ""
+    new_array = Array.new
+    new_array << string.each_char
+
+  new_array.size.times do |h|
+      next if h != "r"
+      #assign h an index number
+      #record every encounter of when h is "r" along with it's index number.
+      #add one to this index number
+      #go back to the orignal array of charaters or orignal string.
+      #find which character is at the index number of h as "r" plus one
+      #return this charater by putting it into the retrun_this empty string varaible.
+      end
 end
+
+
+
+  pirates_say_arrrrrrrrr("are you really learning Ruby?") # => "eenu"
+  pirates_say_arrrrrrrrr("Katy Perry is on the radio!")   # => "rya"
+  pirates_say_arrrrrrrrr("Pirates say arrrrrrrrr")        # => "arrrrrrrr"
