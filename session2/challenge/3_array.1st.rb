23abadbd0c8432696ef33f10fd8499b279f31b16
abitravers1989 <atravers4007@gmail.com>
2017-07-12 20:49:09 +0100

    Second attempt challenge 3

--- session2/challenge/3_array.1st.rb
+++ session2/challenge/3_array.1st.rb
@@ -7,21 +7,18 @@
 
 class String
 
-  attr_accessor 'string_input'
-  end
 
-  def every_other_char(string_input)
+
+  def every_other_char (string_1)
 #split the string into an array of it's characters
+  string_2 = string_1.to_a
 
-    string_array = []
-    string_array << string_input.to_char
-#otput every character of a string which has an even index number
-  string_array.each_with_index do |character, index_nr|
-        next if index_nr %2 != 0
+    string_2.each_with_index do |character, index_nr|
+        if index_nr %2 == 0 then
         print "#{character}"
       end
     end
 
 end
 
-"abcdefg".every_other_char
+end
