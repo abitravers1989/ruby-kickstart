c7ff82c27638038c1883887c906d2decb12a2baa
abitravers1989 <atravers4007@gmail.com>
2017-07-14 20:42:42 +0100

    Challenge 3. Sorting directory

--- session2/challenge/3_array.rb
+++ session2/challenge/3_array.rb
@@ -6,50 +6,16 @@
 # "".every_other_char         # => ""
 
 class String
-
-  attr_accessor 'string_input'
+  #have to define it as a string for self to work
+def every_other_char
+  new_string = ""
+  self.each_char.with_index do |char, index|
+    new_string << char if (index.even?)
   end
-
-  def every_other_char(string_input)
-#split the string into an array of it's characters
-
-    string_array = []
-    string_array << string_input.to_char
-#otput every character of a string which has an even index number
-  string_array.each_with_index do |character, index_nr|
-        next if index_nr %2 != 0
-        print "#{character}"
-      end
-    end
+p new_string
 
 end
 
-"abcdefg".every_other_char
-
-
-#OR
-
-
-class String
-
-
-
-  def every_other_char
-#split the string into an array of it's characters
-
-  returns_this = ""
-#not quite sure why it is using the self object here
-  string_2 = self.split("").to_a
-    string_2.each_with_index do |character, index_nr|
-        if index_nr %2 == 0 then
-          returns_this << character
-      end
-    end
-
-    p returns_this
-    end
-
-end
+end 
 
 "abcdefg".every_other_char
-"".every_other_char
