1352e156df6f8b72ac48f5a27fe9c527638f7e61
abitravers1989 <atravers4007@gmail.com>
2017-07-14 19:21:18 +0100

    First attempt at 8

--- session2/challenge/8_array.rb
+++ session2/challenge/8_array.rb
@@ -1,7 +1,29 @@
 # Given an array of elements, return true if any element shows up three times in a row
-# 
+#
 # Examples:
 # got_three? [1, 2, 2, 2, 3]  # => true
 # got_three? ['a', 'a', 'b']  # => false
 # got_three? ['a', 'a', 'a']  # => true
 # got_three? [1, 2, 1, 1]     # => false
+
+def got_three?(arr)
+i = 0
+k = 1
+g = 2
+arr.length.times do
+if arr[i] == arr[k] && arr[k] == arr[g]
+  return true
+i+=1
+k+=1
+g+=1
+
+else
+  return false
+
+end
+
+end
+
+end
+
+got_three? [1, 2, 2, 2, 3]
