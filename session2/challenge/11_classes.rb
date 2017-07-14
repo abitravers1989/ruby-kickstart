906c2c34dd393f1d55d0c70aaf712141980a9042
abitravers1989 <atravers4007@gmail.com>
2017-07-14 12:45:50 +0100

    First attempt at 11

--- session2/challenge/11_classes.rb
+++ session2/challenge/11_classes.rb
@@ -12,9 +12,47 @@
 # Zero bottles of beer on the wall.
 #
 # Your program should not use ninety-nine output statements!
-# Design your program with a class named BeerSong whose initialize method 
+# Design your program with a class named BeerSong whose initialize method
 # receives a parameter indicating the number of bottles of beer initially on the wall.
 # If the parameter is less than zero, set the number of bottles to zero. Similarly,
 # if the parameter is greater than 99, set the number of beer bottles to 99
 # Then make a public method called print_song that outputs all stanzas from the number of bottles of beer down to zero.
 # Add any additional methods you find helpful.
+
+class BeerSong
+
+  def initialize (nr_bottles)
+    if nr_bottles < 0
+      @nr_bottles = 0
+    elsif nr_bottles > 99
+      @nr_bottles = 99
+    else
+      @nr_bottles = nr_bottles.to_i
+    end
+  end
+
+  def print_song
+    while @nr_bottles != 0
+    puts @nr_bottles.to_s + 'botles of beer on the wall'
+    puts @nr_bottles.to_s + 'bottles of beer'
+    @nr_bottles = @nr_bottles - 1
+    puts "takes one down, pass it around"
+
+    if @nr_bottles == 1
+      puts @nr_bottles.to_s + 'bottle of beer on the wall'
+    else
+      puts @nr_bottles.to_s + 'bottle of beer on the wall'
+    end
+
+    if @nr_bottles == 0
+      puts "END"
+    end
+
+  end
+end
+
+  public :print_song
+
+end
+
+BeerSong.new 77
