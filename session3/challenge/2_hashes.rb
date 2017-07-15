797026812ec93e2e406589051bd18d13eaf36fff
abitravers1989 <atravers4007@gmail.com>
2017-07-15 12:23:46 +0100

    Next stage, working out how to increment the arrays

--- session3/challenge/2_hashes.rb
+++ session3/challenge/2_hashes.rb
@@ -17,9 +17,11 @@ key array   upto(nr_given)
 value array up_to(nr_given) next if (i %2) != 0
 #need to work out how to increment values and if up_to is method
 
+#creating the arrays for the key and values
+keys=
+Array(0..nr_given).map {|x| if x.even? puts "x"}
 
-
-#useful info 
+#useful info
 Hash.new { |this_hash, key| this_hash[key] = Array.new }
 hash = Hash[ *[ Array('a'..'j') , Array(1..10) ].transpose.flatten ] # just populating
 hash                                  # => {"a"=>1, "b"=>2, "c"=>3, "d"=>4, "e"=>5, "f"=>6, "g"=>7, "h"=>8, "i"=>9, "j"=>10}
