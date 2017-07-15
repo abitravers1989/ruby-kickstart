c2e2d6f9b79f3418663665c3ce8dfa2714c9c793
abitravers1989 <atravers4007@gmail.com>
2017-07-15 17:05:19 +0100

    Thinking about 5

--- session3/challenge/5_blocks.rb
+++ session3/challenge/5_blocks.rb
@@ -16,3 +16,15 @@
 #   order << i
 # end
 # order # => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+
+
+def spiral_access (n, &block)
+
+#example of recision ::::
+def fact (number)
+  if number <= 1
+    number
+  else
+    number * fact(number-1)
+  end
+end
