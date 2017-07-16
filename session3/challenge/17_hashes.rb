86d0dd8bc2a8f266916263526ed92031e45ef937
abitravers1989 <atravers4007@gmail.com>
2017-07-16 18:46:12 +0100

    Notes added

--- session3/challenge/17_hashes.rb
+++ session3/challenge/17_hashes.rb
@@ -8,6 +8,29 @@
 # print_list_in_reverse head   # >> "1\n2\n"
 
 
+=begin..... NOTES ON HOW TO FLIP AN ARRAY
+array = (1..10).to_a
+
+num = (array.length/2).floor
+
+
+num.times do |i|
+  #setting the temp variable to the first item in the array
+  #because the first time i passes through this array
+  #which hasn't been given an element it is 0, then 1 etc
+  temp = array [i]
+  #so array [i] = array index 0, the first value
+  #not sure why it needs to be array.length-1-i not just -i
+  #oooo because on the first pass through i is zero so we need to -1 because we want the 9th element
+  #which is the last element because arrays start at 0
+  array[i] = array[array.length-1-i]
+  array[array.length-1-i] = temp #which had been set to the first element
+end
+
+p array
+
+=end
+
 
 def reverse(head)
     if head == nil then retrun "empty"
